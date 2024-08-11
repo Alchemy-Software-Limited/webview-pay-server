870a1c5d65d04c6e752f6a73441a926aca0ab419
Sadman Sakib <sakibsadman108@gmail.com>
2024-08-11 18:55:30 +0600

    Update index.js

--- app/index.js
+++ app/index.js
@@ -62,6 +62,12 @@ app.use(cors({
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 }));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,PUT,PATCH,POST,DELETE");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.use(express.json())
 app.use(cookieParser())
 app.use(express.urlencoded({ extended: true }))
