ef24820c48b997311122f810a0b0e9e3417cd237
Sadman Sakib <sakibsadman108@gmail.com>
2024-08-11 17:07:38 +0600

    Update index.js

--- app/index.js
+++ app/index.js
@@ -56,13 +56,12 @@ const {
 } = require('../middlewares/error')
 
 const app = express()
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET,PUT,PATCH,POST,DELETE");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-})
-app.use(cors())
+
+app.use(cors({
+  origin: 'https://webview-pay.vercel.app',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+}));
 app.use(express.json())
 app.use(cookieParser())
 app.use(express.urlencoded({ extended: true }))
