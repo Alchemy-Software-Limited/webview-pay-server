a6632596af5bda85e6c58ace216dd4da001f6559
Sadman Sakib <sakibsadman108@gmail.com>
2024-08-12 18:04:12 +0600

    update:unsubscribe socket

--- server.js
+++ server.js
@@ -1,37 +1,32 @@
-const db_connect = require("./db/connection")
-const app = require("./app");
-const http = require("http");
-const httpServer = http.createServer(app);
-const config = require("./config/index");
-const {getSubscription} = require("./lib/customer");
+const db_connect = require('./db/connection')
+const app = require('./app')
+const http = require('http')
+const httpServer = http.createServer(app)
+const config = require('./config/index')
+const { getSubscription } = require('./lib/customer')
 
-db_connect && httpServer.listen(config.db_port, async () => {
-	console.log("Server is listening to port: ", config.db_port);
-});
-
+db_connect &&
+    httpServer.listen(config.db_port, async () => {
+        console.log('Server is listening to port: ', config.db_port)
+    })
 
 // Initialize Socket.IO after the server starts listening
-const io = require("socket.io")(httpServer, {
-	cors: {
-		origin: "*",
-		methods: ["GET", "POST"]
-	}
-});
-io.on("connection", function (socket) {
-	socket.on("get:subscription", async (data) => {
-		const subscription = await getSubscription(data);
-		const userInfo = {
-			...data,
-			subscription
-		}
-		io.emit("user:subscription", userInfo);
-	})
-	socket.on("unsubscribe", (data) => {
-		io.emit("user:unsubscribe", {...data.data})
-	})
+const io = require('socket.io')(httpServer, {
+    cors: {
+        origin: '*',
+        methods: ['GET', 'POST'],
+    },
 })
-
-
-
-
-
+io.on('connection', function (socket) {
+    socket.on('get:subscription', async (data) => {
+        const subscription = await getSubscription(data)
+        const userInfo = {
+            ...data,
+            subscription,
+        }
+        io.emit('user:subscription', userInfo)
+    })
+    socket.on('unsubscribe', (data) => {
+        io.emit(`user:unsubscribe:${data.data.email}`, { ...data.data })
+    })
+})
